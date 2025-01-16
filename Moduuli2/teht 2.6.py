c734535dfd2a85e48709201070d41f680dc93c4c
lauri <lauri.labbas@metropolia.fi>
2025-01-16 13:20:50 +0200

    Update hello.py

---
+++ Moduuli2/teht 2.6.py
@@ -0,0 +1,8 @@
+import random
+
+satunnaiset_luvut = [random.randint(0, 9) for _ in range(3)]
+satunnaiset_luvut1 = [random.randint(1,6) for _ in range(4)]
+print(satunnaiset_luvut)
+print(satunnaiset_luvut1)
+
+
