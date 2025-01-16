319070c7e20a13080dea4c490765fcab282d333b
lauri <lauri.labbas@metropolia.fi>
2025-01-16 15:20:37 +0200

    Update hello.py

---
+++ moduuli 3/teht 3.1.py
@@ -0,0 +1,13 @@
+Kuha = float(input("Anna kuhan mitta (cm): "))
+B = 37 - Kuha
+if Kuha<37:
+     print(f"laske kuha takaisin järveen, kuha on {B} cm liian lyhyt")
+else:
+     print("Kuha on täysimittainen")
+
+
+
+
+
+
+
